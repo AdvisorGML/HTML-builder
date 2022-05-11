ec5d44cd47f2abb855cf05bacd9ca874ac43a760
AdvisorGML <advisor@tut.by>
2022-05-11 14:15:02 +0300

    feat: add complite task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,7 +1,24 @@
 const path = require('path');
 const fs = require('fs');
-const { stdin } = require('process');
-console.log('HI!');
-stdin.on('str', (str) => {
-  console.log(str);
+const { stdin: input } = require('process');
+const readline = require('readline');
+
+const rl = readline.createInterface({ input });
+
+console.log('Enter the text. To complete the input, enter "exit" or click "Ctrl+c"');
+
+const file = path.join(__dirname, 'text.txt');
+fs.createWriteStream(file, 'utf-8');
+
+rl.on('line', (line) => {
+  if (line === 'exit') {
+    console.log('Good buy!!!');
+    process.exit(0);
+  }
+  fs.appendFile(file, `${line}\n`, (error) => { if (error) throw error; });
+});
+
+process.on('SIGINT', () => {
+  console.log('Good buy!!!');
+  process.exit(0);
 });
